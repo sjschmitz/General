656c51c312ecca617b36e699f7472daebf742d77
Steven Schmitz <120681809+sjschmitz@users.noreply.github.com>
2023-09-02 15:29:40 -0600

    Update PayetteQuery.py
    added comments

--- PayetteQuery.py
+++ PayetteQuery.py
@@ -14,7 +14,7 @@ df.info() #instantaneous value is column 00060, indexed at 0
 
 len(df.index) #quantity of rows in Pandas dataframe object
 
-flow_values = df['00060']
+flow_values = df['00060'] #00060 is the identifier for flow information within dataframe
 flow_index = df.index.tolist()
 flow_index = pd.to_datetime(flow_index)
 flow_index = flow_index.to_pydatetime() #a list of datetime objects that correspond to flow_values objects
@@ -29,7 +29,7 @@ ax.set(ylabel='Flow (cfs)', title='North Fork Payette at Banks')
 plt.fill_between(x=flow_index,y1=2200,y2=ymax,color='g',alpha=0.10)
 plt.fill_between(x=flow_index,y1=1500,y2=0,color='r',alpha=0.10)
 
-todays_flow = int(flow_values[-1])
+todays_flow = int(flow_values[-1]) #most recent flow value
 flowstr = (r'The current flow is %s cfs!' % (todays_flow))
 props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
 ax.text(.25,0.7,flowstr,fontsize='small',verticalalignment='top',transform=ax.transAxes,bbox=props)
