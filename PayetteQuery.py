caf638ef988e171f802146eb2f1900bca2e95a0c
Steven Schmitz <120681809+sjschmitz@users.noreply.github.com>
2023-09-02 15:23:45 -0600

    Update PayetteQuery.py

--- PayetteQuery.py
+++ PayetteQuery.py
@@ -1,8 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[65]:
-
 
 import dataretrieval.nwis as nwis
 import pandas as pd
@@ -10,50 +5,19 @@ from datetime import datetime as datetime
 from datetime import date
 import matplotlib.pyplot as plt
 
-
-# In[66]:
-
-
-#pd.set_option('display.max.columns',None)
-
-
-# In[75]:
-
-
 site = '13246000' #North Fork at Banks, ID
 today = date.today()
 instant_values = []
 
-
-# In[68]:
-
-
 df, md = nwis.get_iv(sites=site,start='2023-5-1',end=today,multi_index=True,wide_format=True)
-
-
-# In[77]:
-
-
 df.info() #instantaneous value is column 00060, indexed at 0
 
-
-# In[71]:
-
-
 len(df.index) #quantity of rows in Pandas dataframe object
-
-
-# In[106]:
-
 
 flow_values = df['00060']
 flow_index = df.index.tolist()
 flow_index = pd.to_datetime(flow_index)
 flow_index = flow_index.to_pydatetime() #a list of datetime objects that correspond to flow_values objects
-
-
-# In[130]:
-
 
 fig, ax = plt.subplots()
 ax.plot(flow_index,flow_values)
@@ -74,10 +38,6 @@ ax.grid()
 fig.autofmt_xdate()
 ax.legend()
 plt.show()
-
-
-# In[ ]:
-
 
 
 
